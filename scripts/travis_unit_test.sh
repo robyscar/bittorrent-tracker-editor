c1067dd91ae88568d9867b0c17d67d867d6ecb27
Gerry Ferdinandus <gerry_ferdinandus@hotmail.com>
2020-06-24 06:24:43 +0200

    add error show in xvfb

--- scripts/travis_unit_test.sh
+++ scripts/travis_unit_test.sh
@@ -16,7 +16,7 @@ then
 
   echo 'Start: Test SSL connection'
   set -e
-  xvfb-run enduser/trackereditor -TEST_SSL
+  xvfb-run -e /dev/stdout enduser/trackereditor -TEST_SSL
   set +e
   echo 'Succsess: Test SSL connection'
   
